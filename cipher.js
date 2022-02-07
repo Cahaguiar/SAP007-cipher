71aec4b108543d76101c07cc1cc1c91973cfd8f3
Camila Aguiar <camilaguiar73@gmail.com>
2022-02-06 23:43:07 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,52 @@
+const cipher = {// ...função cifrar
+  encode(offSet,senhaParaCifrar){
+  
+
+    let quantidadeDeletras= senhaParaCifrar.length;
+    let resultadoCifra="";
+
+    for(let i =0;i<quantidadeDeletras;i++) {
+      let codigoASC = senhaParaCifrar.charCodeAt(i); //passando a senha para linguagem unicode
+      let codigoA= 65;
+      let codigoZ= 90;
+      if (codigoASC>=65 && codigoASC <=codigoZ){
+      let cifrar= (codigoASC-codigoA +offSet) % 26 + codigoA;
+      
+      resultadoCifra= resultadoCifra + String.fromCharCode(cifrar)
+      }
+      
+      //resultadoCifra= resultadoCifra + String.fromCharCode(cifrar)
+
+    
+    }
+    
+    return resultadoCifra;
+
+
+  },
+
+  decode(offsetdecode, senhaParaDecifrar){
+  
+
+  let quantidadeDeletras2= senhaParaDecifrar.length;
+  let resultadoDecifra= "";
+
+  for(let i=0; i<quantidadeDeletras2;i++) {
+      let codigoASC2= senhaParaDecifrar.charCodeAt(i);
+      let codigoA= 65;
+      let codigoZ= 90;
+      if (codigoASC2>=65 && codigoASC2 <=codigoZ){
+        let decifrar= (codigoASC2 + codigoA - offsetdecode) % 26 + codigoA;
+        
+        resultadoDecifra = resultadoDecifra + String.fromCharCode(decifrar)
+      }
+  }
+  
+  return resultadoDecifra;
+  }
+
+}
+
+
+
+export default cipher;
---
+++ index.js
@@ -0,0 +1,57 @@
+import cipher from './cipher.js';
+
+const offSet= document.getElementById("offset-encode");
+
+const senhaParaCifrar= document.getElementById("password1");
+
+const btnCifrar = document.getElementById("btnCifrar");
+
+const senhaCifrada= document.getElementById("password_cipher");
+    
+function codificarButton() {//função para cifrar senha//
+    const offSetvalue= offSet.valueAsNumber;//pegando o valor dentro do input, o AsNumber eu informo que é um número//
+    const senhaParaCifrarvalue= senhaParaCifrar.value;// input.value (pega o valor dentro input)
+    senhaCifrada.value = cipher.encode(offSetvalue, senhaParaCifrarvalue)
+    
+}
+btnCifrar.addEventListener("click", codificarButton);
+   //chamando evento//
+
+
+const offsetdecode= document.getElementById("offset-decode");
+
+const senhaParaDecifrar= document.getElementById("password2");
+
+const btnDecifrar= document.getElementById("btndecifrar");
+
+const senhaDecifrada= document.getElementById("password_deciphered")
+
+function decodificarButton() {
+    const offSetvalue2 = offsetdecode.valueAsNumber;
+    const senhaParaDecifrarvalue2= senhaParaDecifrar.value;
+    
+    senhaDecifrada.value = cipher.decode(offSetvalue2, senhaParaDecifrarvalue2)
+
+}
+
+
+btnDecifrar.addEventListener("click", decodificarButton);
+
+
+
+
+//const offSetvalue= offSet.valueAsNumber//pegando o valor dentro do input, o AsNumber eu informo que é um número//
+    //const senhaParaCifrarvalue= password1.value// input.value (pega o valor dentro input)
+
+
+
+
+
+
+
+
+
+
+
+
+
